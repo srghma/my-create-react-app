8b0d75c7ac25b7ccd7bd8d568c1f8e03ef0ea994
BjornMelgaard <melgaardbjorn@gmail.com>
2018-05-05 21:30:28 +0300

    feat: update

--- src/components/Result/default/presentation.js
+++ src/components/Result/default/presentation.js
@@ -1,32 +1,35 @@
 import React from 'react'
 import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table'
+import { Grid } from 'material-ui'
 import { mapIndexed } from 'srghma-react-helpers'
 
 const Default = ({
   classes,
   result,
 }) => (
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell className={classes.header}>
-          Назва спеціальності
-        </TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {
-        mapIndexed(
-          (name, i) => (
-            <TableRow key={i}>
-              <TableCell className={classes.cell}>{name}</TableCell>
-            </TableRow>
-          ),
-          result,
-        )
-      }
-    </TableBody>
-  </Table>
+  <Grid container style={{ maxHeight: '500px', overflow: 'auto' }}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell className={classes.header}>
+            Назва спеціальності
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {
+          mapIndexed(
+            (name, i) => (
+              <TableRow key={i}>
+                <TableCell className={classes.cell}>{name}</TableCell>
+              </TableRow>
+            ),
+            result,
+          )
+        }
+      </TableBody>
+    </Table>
+  </Grid>
 )
 
 export default Default
